c522557e38300c4a6dfc1b1dcde753a9a78cb7a8
Jordan Paul <jordan@getpixeltag.com>
2025-05-12 22:34:18 -0700

    updating 1 more place

--- components/executions/execution-details-panel.tsx
+++ components/executions/execution-details-panel.tsx
@@ -81,7 +81,7 @@ export function ExecutionDetailsPanel({ execution, onClose }: ExecutionDetailsPa
           </div>
           <div className="bg-gray-50 p-4 rounded-lg">
             <h3 className="text-sm font-medium text-gray-500">EXECUTION TIME</h3>
-            <p className="dark:text-gray-400">{execution.execution_time ? `${(execution.execution_time / 1000).toFixed(2)} seconds` : "Running..."}</p>
+            <p className="dark:text-gray-400">{execution.execution_time ? `${(execution.execution_time).toFixed(2)} seconds` : "Running..."}</p>
           </div>
           <div className="bg-gray-50 p-4 rounded-lg">
             <h3 className="text-sm font-medium text-gray-500">DATE</h3>
