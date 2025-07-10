4694a55f60f26e03d364614367a62a37e02915c1
Jordan Paul <jordan@getpixeltag.com>
2025-07-10 13:45:12 -0700

    updating input section

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -37,4 +37,20 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
 )
 Input.displayName = "Input"
 
-export { Input }
+const InputTextArea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<"textarea">>(
+  ({ className, ...props }, ref) => { 
+    return (
+      <textarea
+        className={cn(
+          "flex h-20 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+InputTextArea.displayName = "InputTextArea"
+
+export { Input, InputTextArea }
