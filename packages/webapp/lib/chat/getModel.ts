113fbe1975b901a936a89102a45abbdbb5a8c3e1
Jordan Paul <jordan@getpixeltag.com>
2025-09-09 10:42:26 -0700

    wip

--- packages/webapp/lib/chat/getModel.ts
+++ packages/webapp/lib/chat/getModel.ts
@@ -3,13 +3,13 @@ import { createOpenRouter } from '@openrouter/ai-sdk-provider';
 import { openai } from '@ai-sdk/openai';
 
 // Returns an AI model instance based on environment configuration
-const getModel = () => {
+const getModel = (name = process.env.OPENROUTER_MODEL || 'deepseek/deepseek-chat-v3.1') => {
 
   const openrouter = createOpenRouter({
     apiKey: `${process.env.OPENROUTER_API_KEY}`,
   });
-  // You can change the model name here as needed
-  return openrouter(process.env.OPENROUTER_MODEL || 'deepseek/deepseek-chat-v3.1'); // openai/gpt-oss-120b deepseek/deepseek-chat-v3.1 deepseek/deepseek-chat-v3-0324 | google/gemini-2.0-flash-001
+  // You can change the model name here as needed`
+  return openrouter(name); // google/gemini-2.0-flash-001 openai/gpt-oss-120b deepseek/deepseek-chat-v3.1 deepseek/deepseek-chat-v3-0324 | google/gemini-2.0-flash-001
   if (process.env.OPENROUTER_API_KEY) {
     
   } else if (process.env.OPENAI_API_KEY) {
